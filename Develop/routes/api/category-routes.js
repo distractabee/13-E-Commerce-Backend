9d2a6eaeb42afb1515717f9c8922742dab62b9bc
distractabee <jssclgh@gmail.com>
2024-05-16 18:08:29 -0500

    updated index models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,20 +5,39 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [Product]
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+      // be sure to include its associated Products
+    include: [Product]
+  })
+  .then((category) => res.json(category))
+  .catch((err) => res.status(400).json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((newCategory) => res.status(200).json(newCategory))
+  .catch((err) => res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    
+  })
 });
 
 router.delete('/:id', (req, res) => {
